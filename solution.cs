6d5dfa079a8ed0be1d53d835b01fb9528850defd
Chaitanya Datar <datar.chaitanya@gmail.com>
2026-03-18 18:49:28 +0000

    Create solution.cs

---
+++ solution.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public string MakeFancyString(string s) {
+        if(s.Length < 3) return(s);
+
+        StringBuilder sb = new StringBuilder(s.Substring(0, 2));
+        for(int i = 2; i < s.Length; i++){
+            if(s[i - 2] == s[i - 1] && s[i - 1] == s[i]) continue;
+            sb.Append(s[i]);
+        }
+
+        return(sb.ToString());
+    }
+}
